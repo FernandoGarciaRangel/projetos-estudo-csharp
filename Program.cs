2efede1b6f7f42c7e580e5aeffd6a8399a50f889
Fernando Garcia <108248511+FernandoGarciaRangel@users.noreply.github.com>
2024-12-09 03:06:58 -0300

    Upado arquivo de calculador
    Projeto criado para aprender os fundamentos do C#.

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿class Program{
+    static void Main(string[] args)
+    {
+        Menu();
+    }
+    static void Menu(){
+        Console.Clear();
+
+        Console.WriteLine("Opcões disponíveis");
+        Console.WriteLine("1 - Soma");
+        Console.WriteLine("2 - Subtração");
+        Console.WriteLine("3 - Multiplicação");
+        Console.WriteLine("4 - Divisão");
+        Console.WriteLine("0 - Encerrar");
+
+        Console.WriteLine("-----------------------------");
+
+        Console.WriteLine("Digite uma opção:");
+        short res = short.Parse(Console.ReadLine());
+
+        switch(res){
+            case 1: Soma(); break;
+            case 2: Subtracao(); break;
+            case 3: Multiplicacao(); break;
+            case 4: Divisao(); break;
+            case 0: System.Environment.Exit(0); break;
+            default: Menu(); break;
+        }
+    }
+    static void Soma() {
+
+        Console.Clear();
+
+        Console.WriteLine("Digite o primeiro valor:");
+        double primeiroValor = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo valor:");
+        double segundoValor = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("");
+
+        double resultado = primeiroValor + segundoValor;
+
+        Console.WriteLine($"O resultado da sua soma é: {resultado}");
+        Console.ReadKey();
+        Menu();
+    }
+
+    static void Subtracao(){
+        Console.Clear();
+
+        Console.WriteLine("Digite o Primeiro valor:");
+        double primeiroValor = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo valor:");
+        double segundoValor = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("");
+
+
+        double resultado = primeiroValor - segundoValor;
+
+        Console.WriteLine($"O resultado da subtração é {resultado}");
+        Console.ReadKey();
+        Menu();
+    }
+
+    static void Multiplicacao(){
+        Console.Clear();
+
+        Console.WriteLine("Digite o Primeiro valor:");
+        double primeiroValor = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo valor:");
+        double segundoValor = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("");
+
+
+        double resultado = primeiroValor * segundoValor;
+
+        Console.WriteLine($"O resultado da sua multiplicação é {resultado}");
+        Console.ReadKey();
+        Menu();
+    }
+
+    static void Divisao(){
+        Console.Clear();
+
+        Console.WriteLine("Digite o Primeiro valor:");
+        double primeiroValor = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo valor:");
+        double segundoValor = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("");
+
+        double resultado = primeiroValor / segundoValor;
+
+        Console.WriteLine($"O resultado da sua divisão é {resultado}");
+        Console.ReadKey();
+        Menu();
+    }
+
+    }
